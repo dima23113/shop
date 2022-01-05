b195dca690fc02441b0cd8cf15ec45bb76219c0f
dima23113 <namedima4@gmail.com>
2022-01-06 00:16:47 +0300

    add event listener

--- shop/static/js/common.js
+++ shop/static/js/common.js
@@ -1,22 +1,49 @@
 document.addEventListener('DOMContentLoaded', function () {
     function toggleMenu() {
-        $('.menu-toggle').toggleClass('menu-toggle_active')
-        $('.left-menu').toggleClass('left-menu_active')
-        $('.right-auth').toggleClass('right-auth_active')
+        $('.menu-toggle').toggleClass('menu-toggle_active');
+        $('.left-menu').toggleClass('left-menu_active');
+        $('.right-auth').toggleClass('right-auth_active');
+        $('.navbar').toggleClass('navbar_active');
+
     }
 
     $('.menu-toggle').click(function () {
-        toggleMenu()
+        toggleMenu();
     })
 
-    function closeMenu(){
-        $('.menu-toggle').removeClass('menu-toggle_active')
-        $('.left-menu').removeClass('left-menu_active')
-        $('.right-auth').removeClass('right-auth_active')
+    function closeMenu() {
+        $('.menu-toggle').removeClass('menu-toggle_active');
+        $('.left-menu').removeClass('left-menu_active');
+        $('.right-auth').removeClass('right-auth_active');
+        $('.navbar').removeClass('navbar_active');
     }
-    $(document).click(function (e) {
-        if ($(e.target).closest('.menu-container').length) return
-        closeMenu()
-    })
-})
 
+
+    $(document).click(function (e) {
+        if ($(e.target).closest('.menu-container').length) return;
+        closeMenu();
+
+    })
+
+});
+
+$('document').ready(function () {
+    screenClass();
+    $(window).resize(function () {
+        screenClass();
+    });
+
+    function closeMenu() {
+        $('.menu-toggle').removeClass('menu-toggle_active');
+        $('.left-menu').removeClass('left-menu_active');
+        $('.right-auth').removeClass('right-auth_active');
+        $('.navbar').removeClass('navbar_active');
+    }
+
+    function screenClass() {
+        if ($(window).innerWidth() > 1027) {
+            closeMenu()
+        } else {
+        }
+    }
+})
