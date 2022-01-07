aa78305146dfb68fdafe7128a16e7f60672edcf6
dima23113 <namedima4@gmail.com>
2022-01-08 01:33:18 +0300

    add new grid for div logo, brand and description

--- projectshop/settings.py
+++ projectshop/settings.py
@@ -28,6 +28,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = [
+
+    "127.0.0.1",
+]
 
 # Application definition
 
@@ -40,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
     'sorl.thumbnail',
+    'debug_toolbar'
+
 
 ]
 
@@ -51,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware'
 ]
 
 ROOT_URLCONF = 'projectshop.urls'
--- projectshop/urls.py
+++ projectshop/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('shop.urls', namespace='shop')),
     path('admin/', admin.site.urls),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
