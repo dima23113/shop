2797127a1a5cc6926ea3fa7e1e5b7a411635d8af
dima23113 <namedima4@gmail.com>
2022-01-13 11:56:16 +0300

    new app for auth system

--- projectshop/settings.py
+++ projectshop/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'account.apps.AccountConfig',
     'sorl.thumbnail',
     'debug_toolbar'
 
