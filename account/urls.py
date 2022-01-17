796fd81e4cdaa1ae95e3fb87e59fb866c8a8b45d
dima23113 <namedima4@gmail.com>
2022-01-17 18:20:52 +0300

    add pop-up menu, fix account confirm

--- account/urls.py
+++ account/urls.py
@@ -4,7 +4,7 @@ from .views import *
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import views as auth_views
-from django_email_verification import urls as email_urls
+
 
 app_name = 'account'
 
@@ -19,6 +19,5 @@ urlpatterns = [
                                                      success_url=reverse_lazy('account:login')),
          name='password_reset_confirm'),
     path('password-reset/', PasswordResetView.as_view(), name='password_reset'),
-    path('email/', include(email_urls)),
 
 ]
--- projectshop/urls.py
+++ projectshop/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django_email_verification import urls as email_urls
 
 urlpatterns = [
     path('account/', include('account.urls', namespace='account')),
     path('', include('shop.urls', namespace='shop')),
     path('admin/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
+    path('email/', include(email_urls)),
 ]
