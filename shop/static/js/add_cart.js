acb4a7b1e6e5f68c497f57606919b22501188183
dima23113 <namedima4@gmail.com>
2022-01-22 01:25:26 +0300

    add value field for input. add function for set value

--- shop/static/js/add_cart.js
+++ shop/static/js/add_cart.js
@@ -2,20 +2,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     $('#size').click(function () {
-        if ($('#size').attr('class') == 'phone-form form_active') {
-            $('.phone-form').removeClass('form_active');
-        } else {
-            closeForm()
-            $('.phone-form').addClass('form_active');
-        }
-    })
 
-    function closeForm() {
-        $('.phone-form').removeClass('form_active');
-        $('.bio-form').removeClass('form_active');
-        $('.password-change-form').removeClass('form_active');
-        $('.mailing-form').removeClass('form_active');
-        $('.email-form').removeClass('form_active');
-    }
+        atr = String($('#size').attr('class'))
+        var preview = document.getElementById("id_size"); //getElementById instead of querySelectorAll
+        preview.setAttribute("value", atr);
+    })
 
 });
