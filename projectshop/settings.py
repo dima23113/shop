32a2af3256c348441e97500f40bbaab5263a9a6b
dima23113 <namedima4@gmail.com>
2022-01-21 12:15:29 +0300

    add session for cart

--- projectshop/settings.py
+++ projectshop/settings.py
@@ -166,3 +166,6 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'mymail@gmail.com'
 EMAIL_HOST_PASSWORD = 'mYC00lP4ssw0rd'  # os.environ['password_key'] suggested
 EMAIL_USE_TLS = True
+
+
+CART_SESSION_ID = 'cart'
--- shop/context_processors.py
+++ shop/context_processors.py
@@ -3,11 +3,13 @@ from .models import *
 
 def category(request):
     tst = {}
-    for d in Product.objects.all().select_related('subcategory').select_related('category'):
+    for d in Product.objects.all().select_related('subcategory').select_related('category').only('subcategory',
+                                                                                                 'category', 'slug',
+                                                                                                 'image'):
         if d.subcategory in tst.keys():
             pass
         else:
             tst[d.subcategory] = d
     category_img = [v for v in tst.values()]
 
-    return {'categories': Category.objects.all().order_by('id'), 'category_img': category_img}
+    return {'categories': Category.objects.all().order_by('id').only('slug', 'name'), 'category_img': category_img}
