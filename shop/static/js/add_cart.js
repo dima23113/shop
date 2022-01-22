fc634f5fda87cdcf62a71022f08c2a2c4a824b97
dima23113 <namedima4@gmail.com>
2022-01-22 14:47:41 +0300

    Added the ability to set a value in a hidden field size

--- shop/static/js/add_cart.js
+++ shop/static/js/add_cart.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-
-    $('#size').click(function () {
-
-        atr = String($('#size').attr('class'))
-        var preview = document.getElementById("id_size"); //getElementById instead of querySelectorAll
-        preview.setAttribute("value", atr);
-    })
-
+    $(document).ready(function () {
+        $(".size").click(function () {
+            var buttonData = $(this).data("mydata");
+            var preview = document.getElementById("id_size"); //getElementById instead of querySelectorAll
+            preview.setAttribute("value", buttonData);
+        });
+    });
 });
+
+
