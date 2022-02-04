767bd77d56f12bafb31dc08ce4e8efb071519c28
dima23113 <namedima4@gmail.com>
2022-02-05 00:18:25 +0300

    added cache for filter, added filter for size and brand

--- shop/views.py
+++ shop/views.py
@@ -41,7 +41,7 @@ class ProductListByCategory(View):
 
     def get(self, request, *args, **kwargs):
         category = Category.objects.filter(slug=kwargs['slug'])[0]
-        brands = Brand.objects.all().values('name')
+        brands = Brand.objects.all().values('name', 'id')
         sizes = ProductSize.objects.all()
         sizes = list(set([i.name.lower() for i in sizes]))
         products = Product.objects.filter(category=category, available=True)
@@ -106,10 +106,12 @@ class ProductDetail(View):
 class JsonFilterProductView(ListView):
 
     def get_queryset(self):
+        print(self.request.GET.getlist('brand[]'))
+        print(self.request.GET.getlist('size[]'))
         queryset = Product.objects.filter(
             Q(subcategory_type__in=self.request.GET.getlist('product_type[]', '')) |
             Q(brand__in=self.request.GET.getlist('brand[]', '')) |
-            Q(product_sizer__in=self.request.GET.getlist('size[]', ''))
+            Q(product_sizer__name__in=self.request.GET.getlist('size[]', ''))
         ).distinct().select_related('brand').values('name', 'slug', 'brand__name', 'image', 'price')
         return queryset
 
