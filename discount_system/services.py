4bf3cfec43a7bd456fac33d50a16e2300e97c43a
dima23113 <namedima4@gmail.com>
2022-02-21 15:24:29 +0300

    added docstring

--- discount_system/services.py
+++ discount_system/services.py
@@ -1,10 +1,9 @@
-from datetime import datetime
-from decimal import Decimal
 from .models import CategoryDiscount, SubcategoryDiscount, SubcategoryTypeDiscount, BrandDiscount, ProductDiscount
 from shop.models import Product
 
 
 def product_discount_by_category_discount():
+    """Устанавливаем цену со скидкой на товары скидки для категории"""
     for discount in CategoryDiscount.objects.filter(is_active=True):
         for product in Product.objects.filter(available=True, category=discount.category):
             product.price_discount = product.price - (product.price * (discount.amount_of_discount / 100))
@@ -12,6 +11,7 @@ def product_discount_by_category_discount():
 
 
 def product_discount_by_subcategory_discount():
+    """Устанавливаем цену со скидкой на товары скидки для подкатегории"""
     for discount in SubcategoryDiscount.objects.filter(is_active=True):
         for product in Product.objects.filter(available=True, subcategory=discount.subcategory):
             product.price_discount = product.price - (product.price * (discount.amount_of_discount / 100))
@@ -19,6 +19,7 @@ def product_discount_by_subcategory_discount():
 
 
 def product_discount_by_subcategory_type_discount():
+    """Устанавливаем цену со скидкой на товары скидки для типа подкатегории"""
     for discount in SubcategoryTypeDiscount.objects.filter(is_active=True):
         for product in Product.objects.filter(available=True, subcategory_type=discount.subcategory_type):
             product.price_discount = product.price - (product.price * (discount.amount_of_discount / 100))
@@ -26,6 +27,7 @@ def product_discount_by_subcategory_type_discount():
 
 
 def product_discount_by_brand_discount():
+    """Устанавливаем цену со скидкой на бренд скидки для брендов"""
     for discount in BrandDiscount.objects.filter(is_active=True):
         for product in Product.objects.filter(available=True, brand=discount.brand):
             product.price_discount = product.price - (product.price * (discount.amount_of_discount / 100))
