1e755926f7fda62b1fe477ffea7cd19a6eb9e788
dima23113 <namedima4@gmail.com>
2022-03-11 11:18:28 +0300

    added method to send email order information

--- orders/tasks.py
+++ orders/tasks.py
@@ -3,6 +3,10 @@ import uuid
 from celery import shared_task
 from yookassa import Payment
 from orders.models import Order
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 @shared_task
@@ -26,3 +30,15 @@ def confirm_payment():
                 idempotence_key
             )
             print(response.json())
+            send_order_information(order)
+
+
+@shared_task
+def send_order_information(order):
+    order_items = order.items.all()
+    html_template = render_to_string(template_name='orders/mail_order_detail.html',
+                                     context={'order': order, 'order_items': order_items})
+    html_template = strip_tags(html_template)
+    subject = f'Заказ #{order.id} подтвержден!'
+
+    send_mail(subject, html_template, settings.EMAIL_HOST_USER, [order.email], fail_silently=True)
